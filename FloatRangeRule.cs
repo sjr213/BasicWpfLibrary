36c4763c2d1c700a8ebc9fc8281146f4e9fba832
sjr213 <sjrobles@yahoo.com>
2023-02-25 19:24:23 -0500

    Added FloatRangeRule and UserControlBehavior for IsVisibleChanged command.

---
+++ FloatRangeRule.cs
@@ -0,0 +1,35 @@
+﻿namespace BasicWpfLibrary;
+
+using System;
+using System.Windows.Controls;
+using System.Globalization;
+
+public class FloatRangeRule : ValidationRule
+{
+    public float Min { get; set; }
+    public float Max { get; set; }
+
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        float val = 0f;
+
+
+
+        try
+        {
+            if (((string)value).Length > 0)
+                val = float.Parse((string)value);
+        }
+        catch (Exception e)
+        {
+            return new ValidationResult(false, $"Illegal characters or {e.Message}");
+        }
+
+        if ((val < Min) || (val > Max))
+        {
+            return new ValidationResult(false,
+                $"Please enter an value in the range: {Min}-{Max}.");
+        }
+        return ValidationResult.ValidResult;
+    }
+}
---
+++ UserControlBehavior.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace BasicWpfLibrary
+{
+    public class UserControlBehavior
+    {
+        public static readonly DependencyProperty IsVisibleChangedCommandProperty =
+            DependencyProperty.RegisterAttached("IsVisibleChangedCommand", typeof(ICommand),
+                typeof(UserControlBehavior),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(IsVisibleChangedCommandChanged)));
+
+        private static void IsVisibleChangedCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UserControl element = (UserControl)d;
+
+            element.IsVisibleChanged += element_IsVisibleChanged;
+        }
+
+        static void element_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            UserControl element = (UserControl)sender;
+
+            ICommand command = GetIsVisibleChangedCommand(element);
+
+            command.Execute(e);
+        }
+
+        public static void SetIsVisibleChangedCommand(UIElement element, ICommand value)
+        {
+            element.SetValue(IsVisibleChangedCommandProperty, value);
+        }
+
+        public static ICommand GetIsVisibleChangedCommand(UIElement element)
+        {
+            return (ICommand)element.GetValue(IsVisibleChangedCommandProperty);
+        }
+    }
+}
+
