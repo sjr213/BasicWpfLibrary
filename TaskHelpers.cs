1389c16d55cf08be8987d1587daec4151c6253aa
sjr213 <sjrobles@yahoo.com>
2023-10-31 20:03:44 -0400

    Added AsyncCommand and used for OnOpen for fractal and movie files.

---
+++ TaskHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BasicWpfLibrary;
+
+public static class TaskHelpers
+{
+    // https://stackoverflow.com/questions/63245986/c-sharp-waiting-for-all-async-tasks-to-complete-when-calling-from-a-non-async-me
+    public static async void FireAndForgetSafeAsync(this Task task, Action<Exception>? handleErrorAction = null)
+    {
+        try
+        {
+            await task.ConfigureAwait(true);
+        }
+        catch (Exception ex)
+        {
+            handleErrorAction?.Invoke(ex);
+        }
+    }
+}
+
