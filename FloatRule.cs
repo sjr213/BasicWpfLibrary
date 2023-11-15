08102cabcc280562de087350cf5661cfe772c3e8
sjr213 <sjrobles@yahoo.com>
2023-11-14 19:46:16 -0500

    Added FloatRule for plain validation.

---
+++ FloatRule.cs
@@ -0,0 +1,25 @@
+﻿namespace BasicWpfLibrary;
+
+using System;
+using System.Windows.Controls;
+using System.Globalization;
+
+public class FloatRule : ValidationRule
+{
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        float val = 0f;
+
+        try
+        {
+            if (((string)value).Length > 0)
+                val = float.Parse((string)value);
+        }
+        catch (Exception e)
+        {
+            return new ValidationResult(false, $"Illegal characters or {e.Message}");
+        }
+
+        return ValidationResult.ValidResult;
+    }
+}
