e7ea799dfa2db761322d8ea0c1228539e77401b4
Steve Robles <sjrobles@yahoo.com>
2026-07-23 20:53:26 -0400

    Add generic RelayCommand implementation.

--- RelayCommand.cs
+++ RelayCommand.cs
@@ -36,3 +36,43 @@ public class RelayCommand : ICommand
     public void Execute(object? parameter) { _execute(parameter); }
     #endregion // ICommand Members 
 }
+
+
+public class RelayCommand<T> : ICommand
+{
+    private readonly Action<T> _execute;
+    private readonly Func<T, bool>? _canExecute;
+
+    public RelayCommand(Action<T> execute, Func<T, bool>? canExecute = null)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        if (_canExecute == null) return true;
+        if (parameter == null && typeof(T).IsValueType) return false;
+
+        if(parameter is not T)
+        {
+            throw new ArgumentException($"Parameter is not of type {typeof(T).Name}", nameof(parameter));
+        }
+        return _canExecute((T)parameter);
+    }
+
+    public void Execute(object? parameter)
+    {
+        if (parameter is not T)
+        {
+            throw new ArgumentException($"Parameter is not of type {typeof(T).Name}", nameof(parameter));
+        }
+        _execute((T)parameter);
+    }
+}
